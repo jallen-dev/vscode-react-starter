06e2c4723373369352821c1f4c8140a1d4dda417
Justin Allen <jallen@jallen.dev>
2023-08-10 22:08:00 -0400

    Properly attribute licensed code, include entire notice

--- webview-ui/src/utils/vscode.ts
+++ webview-ui/src/utils/vscode.ts
@@ -76,3 +76,30 @@ class VSCodeAPIWrapper {
 
 // Exports class singleton to prevent multiple invocations of acquireVsCodeApi.
 export const vscode = new VSCodeAPIWrapper()
+
+/*
+  Substantial portions of this code were taken from vscode-webview-ui-toolkit-samples
+  https://github.com/microsoft/vscode-webview-ui-toolkit-samples/blob/6131aa6b49a49434003529e7b0fb7bff703dd02d/frameworks/hello-world-react-vite/webview-ui/src/utilities/vscode.ts
+
+  MIT License
+
+  Copyright (c) Microsoft Corporation.
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE
+  */
